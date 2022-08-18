1e722f110efaab5e28b210c7099084f606f22b16
vikashkritant <vikashkritant@gmail.com>
2022-08-18 21:11:43 +0530

    Add project files.

---
+++ Excel-Reader/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Excel_Reader
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Excel-Reader/Function1.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Excel_Reader
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([BlobTrigger("exceldata/{name}", Connection = "StorageConnection")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            try
+            {
+                var ConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                // Setup the connection to the storage account
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+                // Connect to the blob storage
+                CloudBlobClient serviceClient = storageAccount.CreateCloudBlobClient();
+                // Connect to the blob container
+                CloudBlobContainer container = serviceClient.GetContainerReference("exceldata");
+                // Connect to the blob file
+                CloudBlockBlob blob = container.GetBlockBlobReference(name);
+                
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    blob.DownloadToStreamAsync(memoryStream).GetAwaiter().GetResult();
+                    memoryStream.Position = 0;
+                    using (var reader = new StreamReader(memoryStream))
+                    using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+                    {
+                        var records = csv.GetRecords<Employee>();
+                        foreach (Employee item in records)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
